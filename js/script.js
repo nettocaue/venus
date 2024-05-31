09e4838b4ec764c8d6b16f7fc28338bbc9cea6be
nettocaue <caue.netto234@gmail.com>
2024-05-31 16:36:40 -0300

    Adiciona modal de newsletter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Função para mudar o title do site quando o usuário estiver em outra aba
 document.addEventListener("visibilitychange", function() {
     if (document.visibilityState === 'hidden') {
         document.title = "🛍️ Volte para suas compras";
@@ -6,6 +7,50 @@ document.addEventListener("visibilitychange", function() {
         document.title = "Venus Cosméticos";
     }
 });
+
+// Função para exibir ou remover a visualização do modal de newsletter
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        var modal = document.getElementById('modal');
+        modal.style.display = 'flex';
+
+        var span = document.getElementsByClassName('close')[0];
+        span.onclick = function() {
+            modal.style.display = 'none';
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        }
+    }, 2000); 
+});
+
+// Função de validação de email
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('newsletter-form');
+    const emailInput = document.getElementById('email');
+    const emailError = document.getElementById('email-error');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        if (!isValidEmail(emailInput.value)) {
+            emailError.textContent = 'Por favor, insira um e-mail válido.';
+            return;
+        }
+        
+        emailError.textContent = ''; 
+        alert('E-mail cadastrado com sucesso')
+        modal.style.display = 'none';
+    });
+
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+});
+
 
 
 function menuShow() {
